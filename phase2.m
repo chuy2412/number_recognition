5d22728814f59bd6a255f82e88d8331d13642ec1
Jesus Riverajimenez <riveraji@brock.egr.msu.edu>
2013-04-09 10:20:03 -0400

    Fixed values on phase2

--- phase2.m
+++ phase2.m
@@ -47,16 +47,16 @@ X0 = double(reshape(I.train0(1,:),[],1));
 
 %Inputs
 %X = [X0 X1 X2 X3 X4 X5 X6 X7 X8 X9]
-X = [X0 X1 X2];
+X = [X0 X1];
 
 %Desired outputs
 %DDJ = [0 1 2 3 4 5 6 7 8 9]
-DDJ = [0 1 2];    
+DDJ = [0 1];    
 
 ETA = 0.000001;
 [nInputs N] = size(X);
 [nOutputs N] = size(DDJ);
-nHidden = 10000; %Two hidden units
+nHidden = 20; %Two hidden units
 
 %Initialize the weights with small random values
 wji = 0.1*rand(nHidden,nInputs+1);
@@ -78,7 +78,7 @@ Error_Steps=2;
 %Stopping Criteria: 10000 iterations or The error of the last 4 steps is 
 %Less than 4%
 fprintf('Starting Training Phase...\n');
-while((norm(Error_Steps)> 0.04) && (iterations < 20))
+while((norm(Error_Steps)> 0.04) && (iterations < 1000))
     Error_Steps=0; 
     for i = 1:N  
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -155,10 +155,10 @@ fprintf('It took %d iterations \n',iterations);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %Test inputs 
 %test =[X1 X3 X5 X7 X9 X2 X4 X6 X8 X0];
- test = [X1 X0 X2];
+ test = [X1 X0];
 %Desired outputs
 %d = [1 3 5 7 9 2 4 6 8 0];
-d = [1 0 ];             %desired outputs
+d = [1 0];             %desired outputs
 
 fprintf('Starting Testing Phase...\n');
 for i = 1:2 
